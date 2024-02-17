4584bcf35c3f4484830f279d23f354c018e8093d
Ihor Tryndey <igortryndey@gmail.com>
2024-02-17 03:30:33 +0000

    final commit with comments

--- src/interview/SolutionRunner.java
+++ src/interview/SolutionRunner.java
@@ -8,7 +8,6 @@ package interview;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -47,7 +46,7 @@ public class SolutionRunner {
 		 
          // Close the scanner
          scanner.close();
-         
+
          //print out min value using method findMinValue()
          System.out.println("min value " + findMinValue(numbers));
          
@@ -69,7 +68,7 @@ public class SolutionRunner {
 	}
 
 	/**
-	 * Method findMinVAlue which find min value in the given ArrayList
+	 * Method findMinVAlue() which find min value in the given ArrayList
 	 * @param numbers an ArrayList<Integer>
 	 * @return int minValue 
 	 */
@@ -89,7 +88,7 @@ public class SolutionRunner {
 	}
 	
 	/**
-	 * Method findMinVAlue which find max value in the given ArrayList
+	 * Method findMinVAlue() which find max value in the given ArrayList
 	 * @param numbers an ArrayList<Integer>
 	 * @return int maxValue
 	 */
@@ -109,8 +108,8 @@ public class SolutionRunner {
 	}
 	
 	/**
-	 * Method median which find the median of the given ArrayList 
-	 * @param arr1 an ArrayList<Integer>
+	 * Method median() which find the median of the given ArrayList 
+	 * @param arr1 an ArrayList
 	 * @return int median
 	 */
 	public static double median(ArrayList<Integer> arr1) {
@@ -135,73 +134,125 @@ public class SolutionRunner {
 		return median;
 	}
 	
+	/**
+	 * Method avg() that find average value of the given ArrayList
+	 * @param arr ArrayList
+	 * @return int avg
+	 */
 	public static double avg(ArrayList<Integer> arr) {
-		int sum =0;
+		//Initialisation of variable in which stored sum of all element.
+		long sum =0l;
+
+		//add all elements in sum
 		for(int i=0; i< arr.size(); i++) {
 			sum = sum + arr.get(i);
 		}
-		
+		//find average value
 		double avg= (double)sum/arr.size();
 		
 		return avg;
 	}
 	
+	/**
+	 * Method maxAscendingSequence() which find value of max ascending elements in sequence and print this values
+	 * @param arr an ArrayList
+	 * @return int maxCount
+	 */
 	public static int maxAscendingSequence(ArrayList<Integer> arr) {
-		if(arr.size() <= 0) {
+		//if number of elements in ArrayList equals 1 of 0 return arr.size as sequence
+		if(arr.size() <= 1) {
 			return arr.size();
 		} 
 		int value = arr.get(0);		
 		int count = 1;
 		int maxCount = 1;
+		
+		/*
+		 * two variables which help find the start and the end of the set of max elements in ascending sequence
+		 */
 		int start = 0;
 		int end = 0;
+		
+		//find the maxCount of ascending order of elements in a sequence in ArrayList
 		for(int i=1; i<arr.size(); i++) {
+			//addition of count by 1 if next is larger than previous 
 			if(value < arr.get(i) ) {
 				count++;
+				//updating of end if condition is true
 				if(maxCount < count)
 				end = i;
+			//updating the maxCount and restart variable count
 			}else {
 				maxCount = Math.max(maxCount, count);
 				count = 1;				 
 			}
-			
+			//change value to next one
 			value = arr.get(i);
 		}
+		//check the last one sequence if it the largest sequence
 		maxCount = Math.max(maxCount, count);
+		
+		//find the start of the sequence
 		start = end - maxCount +1;
+		
+		//creating the container for result set
 		ArrayList<Integer> result = new ArrayList<>();
+		//fill this ArrayList with elements
 		for(int i=start; i <=end; i++) {
 			result.add(arr.get(i));
 		}
+		//print this result
 		System.out.println(result);
 		return maxCount;
 	}
 	
+	/**
+	 * Method maxDescendingSequence() which find value of max descending elements in sequence and print out this values
+	 * @param arr an ArrayList
+	 * @return int maxCount
+	 */
 	public static int maxDescendingSequence(ArrayList<Integer> arr) {
+		//if number of elements in ArrayList equals 1 of 0 return arr.size as sequence
 		if(arr.size() <= 0) {
 			return arr.size();
 		} 
 		int value = arr.get(0);		
 		int count = 1;
 		int maxCount = 0;
+		
+		/*
+		 * two variables which help find the start and the end of the set of max elements in descending sequence
+		 */
 		int end = 0;
+		int start =0;
+		
+		//find the maxCount of descending order of elements in a sequence in ArrayList
 		for(int i=1; i<arr.size(); i++) {
+			//addition of count by 1 if next is lower than previous
 			if(value > arr.get(i) ) {
 				count++;
+				//updating of end if condition is true
 				if(maxCount < count)
 					end = i;
+			//updating the maxCount and restart variable count
 			}else {
 				maxCount = Math.max(maxCount, count);
 				count = 1;
 			}
+			//change value to next one
 			value = arr.get(i);
 		}
+		//check the last one sequence if it the largest sequence
 		maxCount = Math.max(maxCount, count);
-		int start = end - maxCount +1;
+		//find the start of the sequence
+		start = end - maxCount +1;
+		//creating the container for result set
 		ArrayList<Integer> result = new ArrayList<>();
+		//fill this ArrayList with elements
 		for(int i=start; i <=end; i++) {
 			result.add(arr.get(i));
 		}
+		//print this result
 		System.out.println(result);
 		return maxCount;
 	}
