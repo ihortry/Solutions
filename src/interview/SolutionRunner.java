45fd35eb6b218068887f935ca727f9aeee746f14
Ihor Tryndey <igortryndey@gmail.com>
2024-02-17 02:51:38 +0000

    comments for min max and median methods

--- src/interview/SolutionRunner.java
+++ src/interview/SolutionRunner.java
@@ -1,3 +1,8 @@
+/**
+ * 	@author Ihor Tryndey
+ *  @version 1.0
+ *  @since 2024
+ */
 package interview;
 
 import java.io.File;
@@ -7,15 +12,35 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Ihor Tryndey
+ *
+ * Class SolutionRunner in which I run solutions to the problems
+ */
 public class SolutionRunner {
 
+	/**
+	 * main method
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
+		/*
+		 * creation of container for all integers in file
+		 */
 		ArrayList<Integer> numbers = new ArrayList<>(); 
-		long count = 0l;
+		
+		/*
+		 * Initialisation of file in a variable and declaration of Scanner
+		 */
 		File file =new File("10m.txt");
 		Scanner scanner = new Scanner(file);
-		 while (scanner.hasNextInt()) {
-             count++;           
+		
+		/*
+		 * Here i read all numbers into array
+		 */
+		 while (scanner.hasNextInt()) {           
              int element = scanner.nextInt();
              numbers.add(element);
          }
@@ -23,19 +48,38 @@ public class SolutionRunner {
          // Close the scanner
          scanner.close();
          
+         //print out min value using method findMinValue()
          System.out.println("min value " + findMinValue(numbers));
+         
+         //print out max value using method findMinValue()
          System.out.println("max value " + findMaxValue(numbers));
+         
+         //print out median value using method findMinValue()
          System.out.println("median "+ median(numbers));
+         
+         //print out average value using method findMinValue()
          System.out.println("avg " + avg(numbers));
+         
+         //print out max number of ascending numbers in sequence value using method findMinValue(), also I print out this sequence
          System.out.println("max ascending order "  + maxAscendingSequence(numbers));
+         
+         //print out max number of descending numbers in sequence value using method findMinValue(), also I print out this sequence
          System.out.println("max descending order " + maxDescendingSequence(numbers));
 
 	}
 
+	/**
+	 * Method findMinVAlue which find min value in the given ArrayList
+	 * @param numbers an ArrayList<Integer>
+	 * @return int minValue 
+	 */
 	public static int findMinValue(ArrayList<Integer> numbers) {
+		//in the line below I take the first number as the lowest
 		int minValue = numbers.get(0);
 		
+		//this loop is goes through all elements in ArrayList
 		for(int i=1; i<numbers.size(); i++) {
+			//if value in index i lowest than minValue -> perform swamping 
 			if(minValue >  numbers.get(i)) { 
 				minValue = numbers.get(i);
 			}
@@ -44,9 +88,18 @@ public class SolutionRunner {
 		return minValue;
 	}
 	
+	/**
+	 * Method findMinVAlue which find max value in the given ArrayList
+	 * @param numbers an ArrayList<Integer>
+	 * @return int maxValue
+	 */
 	public static int findMaxValue(ArrayList<Integer> numbers) {
+		//in the line below I take the first number as the largest
 		int maxValue = numbers.get(0);
+		
+		//this loop is goes through all elements in ArrayList
 		for(int i=1; i<numbers.size();i++) {
+			//if value in index i larger than maxValue -> perform swamping 
 			if(maxValue < numbers.get(i)) {
 				maxValue = numbers.get(i);
 			}
@@ -55,13 +108,26 @@ public class SolutionRunner {
 		return maxValue;
 	}
 	
+	/**
+	 * Method median which find the median of the given ArrayList 
+	 * @param arr1 an ArrayList<Integer>
+	 * @return int median
+	 */
 	public static double median(ArrayList<Integer> arr1) {
+		//Creation an new ArrayList to sort it and not change data in original one
 		ArrayList<Integer> arr = new ArrayList<>(arr1);
+		//Sort this array, because first we should order the data to find median
 		Collections.sort(arr);
 		int arrLength = arr.size();
 		double median;
+		
+		//algorithm for list of integers which have even number of elements
 		if(arrLength%2 == 0) {
+			/*
+			 * for even number of elements, we should find two centre elements
+			 */
 			median = (double)((arr.get(arrLength-1)/2) + arr.get(arrLength/2))/2;
+		//algorithm for list of integers which have odd number of elements
 		}else {
 			median =arr.get(arrLength/2);
 		}
